ccc5743ea3f5b31a0d5749c164cf28ec5a6e5914
Sebastian Weiss <sebastian.weiss2@ikea.com>
2021-07-12 15:29:30 +0200

    create http observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StarComponent } from "./shared/star.component";
     StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
