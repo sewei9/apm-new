b8ba6045a970977ffec8c50590be30533a35a948
Sebastian Weiss <sebastian.weiss2@ikea.com>
2021-07-12 14:34:45 +0200

    build, register and inject services (products)

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import {ProductService} from "./product.service";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
@@ -23,37 +25,11 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
-
-  performFilter(filterBy: string): IProduct[]{
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: IProduct) =>
-    product.productName.toLocaleLowerCase().includes(filterBy));
-  }
+  products: IProduct[] = [];
 
   ngOnInit(): void {
-    this.listFilter =  'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts =  this.products;
   }
 
   toggleImage(): void {
@@ -62,5 +38,13 @@ export class ProductListComponent implements OnInit {
 
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
+  }
+
+  constructor(private productService: ProductService) {}
+
+  performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
   }
 }
